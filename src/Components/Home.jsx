6fbb7a6dd8ca459c0bb8a67d95eba340b2f677d9
unknown <fahadkabooro.fk@gmail.com>
2023-08-05 15:23:08 +0500

    Using formik and yup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: "center", fontSize: "4rem" }}>
+        Congr8s 🎉 You are Registered
+      </div>
+    );
+  }
+}
+export default Home;
